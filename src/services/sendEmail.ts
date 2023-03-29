cf42c09b617206e775d23c50a4b2d1d92e5508b9
Mykyta Matvieiev <matveev.nikita96@gmail.com>
2023-03-29 17:56:41 +0300

    forgot

--- src/services/sendEmail.ts
+++ src/services/sendEmail.ts
@@ -1,6 +1,6 @@
 const PORT = process.env.PORT ?? 3000
 const FRONT = process.env.F_PORT
-
+const DOMAIN = process.env.B_DOMAIN
 export class Mail {
   mailData: object;
   constructor() {
@@ -13,9 +13,9 @@ export class Mail {
       to: [`${email}`],
       subject: 'Hello, please Verify your account',
       text: ` Hello, ${userData.firstName} ${userData.lastName}, please verify you account!
-       Please, click this link: href="http://localhost:${PORT}/?token=${userData.token}">Verify`,
+       Please, click this link: href="http://${DOMAIN}:${PORT}/?token=${userData.token}">Verify`,
       html: ` Hello, ${userData.firstName} ${userData.lastName}, please verify you account!<br>
-      Please, click this link: <a href="http://localhost:${PORT}/?token=${userData.token}">Verify</a>`,
+      Please, click this link: <a href="http://${DOMAIN}:${PORT}/?token=${userData.token}">Verify</a>`,
     };
     return email;
   }
@@ -26,9 +26,9 @@ export class Mail {
       to: [`${email}`],
       subject: 'Hello, reset password',
       text: ` Hello, ${userData.firstName} ${userData.lastName}, you want to restore your password!
-       Please, click this link: href="http://localhost:${FRONT}/passwordreset?token=${userData.token}">Restore`,
+       Please, click this link: href="http://${DOMAIN}:${FRONT}/passwordreset?token=${userData.token}">Restore`,
       html: ` Hello, ${userData.firstName} ${userData.lastName}, you want to restore your password!<br>
-      Please, click this link: <a href="http://localhost:${FRONT}/passwordreset?token=${userData.token}">Restore</a>`,
+      Please, click this link: <a href="http://${DOMAIN}:${FRONT}/passwordreset?token=${userData.token}">Restore</a>`,
     };
     return email;
   }
